dc4d486977be46ca24b98750fe467a927521ed84
Aurangzeb Malik <auma9171@colorado.edu>
2022-04-20 15:39:46 -0600

    controller is able to locate ball, and determine if goal is occluded or not

--- controllers/heterogenous_robot/heterogenous_robot.py
+++ controllers/heterogenous_robot/heterogenous_robot.py
@@ -3,7 +3,7 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot, Camera, CameraRecognitionObject, Compass, InertialUnit
-import math
+import math, sys
 # create the Robot instance.
 robot = Robot()
 
@@ -21,8 +21,8 @@ MAX_SPEED = 6.28
 leftMotor = robot.getDevice('left wheel motor')
 rightMotor = robot.getDevice('right wheel motor')
 
-Inertialun = robot.getDevice("inertial unit")
-Inertialun.enable(timestep)
+compass = robot.getDevice("compass")
+compass.enable(timestep)
 
 camera = Camera("camera")
 camera.enable(timestep)
@@ -32,7 +32,13 @@ leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
 leftSpeed, rightSpeed = 0, 0
 
-state_machine = {"foundObject" : False, "aligned" : False, "isNearObject" : False, "searchingGoal" : False, "goalFindable" : [False, 0], "foundGoal" : [False, None, None, None]}
+logging = False
+if len(sys.argv)> 1 and sys.argv[1] == "True":
+    logging = True
+
+state_machine = {"foundObject" : False, "aligned" : False, "isNearObject" : False, "searchingGoal" : False, "goalFindable" : [False, None], "foundGoal" : [False, None, None, None]}
+
+deltaTheta = 0
 
 counter = 0
 
@@ -42,6 +48,9 @@ while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
+    n = compass.getValues()
+    rad = -math.atan2(n[0], n[1])
+    pose_theta = rad
     if not state_machine["foundObject"]:
         leftSpeed = -0.5 * MAX_SPEED
         rightSpeed = 0.5 * MAX_SPEED
@@ -56,7 +65,8 @@ while robot.step(timestep) != -1:
             # orange ball: [1.0, 0.54, 0.08]
             if colors == [1, 0.54, 0.08]:
                 state_machine["foundObject"] = True
-                print("found Orange ball")
+                if logging:
+                    print("found Orange ball")
                 # print("[{} == {}?={}, {} == {}?={}, and {} == {}?={}]".format(colors[0], 1, colors[0] == 1, colors[1],
                 #                                                               0.54, colors[1] == 0.54, colors[2], 0.08,
                 #                                                               colors[2] == 0.08))
@@ -73,15 +83,18 @@ while robot.step(timestep) != -1:
                 pos_im = obj.get_position_on_image()
                 if math.isclose(pos_im[0],camera.getWidth()/2):
                     state_machine["aligned"] = True
-                    print("aligned epuck")
+                    if logging:
+                        print("aligned epuck")
                     leftSpeed = 0
                     rightSpeed = 0
                 elif (camera.getWidth() / 2) < pos_im[0] < ((camera.getWidth() / 2) + (camera.getWidth() / 16)):
-                    print("Epuck to the right of center")
+                    if logging:
+                        print("Epuck to the right of center")
                     leftSpeed = 0.05 * MAX_SPEED
                     rightSpeed = -0.05 * MAX_SPEED
                 elif ((camera.getWidth() / 2) - (camera.getWidth() / 16)) < pos_im[0] < (camera.getWidth() / 2):
-                    print("Epuck to the left of center")
+                    if logging:
+                        print("Epuck to the left of center")
                     leftSpeed = -0.05 * MAX_SPEED
                     rightSpeed = 0.05 * MAX_SPEED
                 elif pos_im[0] > ((camera.getWidth() / 2) + (camera.getWidth() / 16)):
@@ -102,31 +115,62 @@ while robot.step(timestep) != -1:
                     state_machine["isNearObject"] = True
                     leftSpeed = 0
                     rightSpeed = 0
-                    state_machine["searchingGoal"] = [True, InertialUnit.getRollPitchYaw()]
-                    print("epuck is at ball")
-                print("Object position: {}".format(objectPostion))
-    elif state_machine["searchingGoal"][0]:
+                    state_machine["searchingGoal"] = True
+                    deltaTheta = 0
+                    if logging:
+                        print("epuck is at ball")
+                # print("Object position: {}".format(objectPostion))
+    elif state_machine["searchingGoal"]:
         # Green RGB
-        # 0.192157
-        # 1
-        # 0.133333
+        # 0.192 1 0.133
+        if not state_machine["foundGoal"][0]:
+            if deltaTheta == 0:
+                previousTheta = pose_theta
+                deltaTheta += 0.0001
+            else:
+                deltaTheta += abs(abs(previousTheta) - abs(pose_theta))
+                previousTheta = pose_theta
+            if logging:
+                print("Current angle: {}, current amount of rotation: {}".format(pose_theta, deltaTheta))
+            if deltaTheta >= 2 * math.pi:
+                leftSpeed = 0
+                rightSpeed = 0
+                if logging:
+                    print("Goal findable: {}".format(state_machine["goalFindable"][0]))
+            else:
 
-        print("Current angle: {}, 360 angle: {}".format(InertialUnit.getRollPitchYaw()[0], state_machine["searchingGoal"][1]))
-        if math.isclose(InertialUnit.getRollPitchYaw()[0], state_machine["searchingGoal"][1]):
-            pass
+                leftSpeed = -0.05 * MAX_SPEED
+                rightSpeed = 0.05 * MAX_SPEED
+                cameraRecogObjects = camera.getRecognitionObjects()
+                # list of all objects detected
+                goalCount = 0
+                for obj in cameraRecogObjects:
+                    # pos = i.get_position()
+                    # x,y,z
+                    colors = obj.get_colors()
+                    pos_im = obj.get_position_on_image()
+
+                    if colors == [0.192, 1, 0.133]:
+                        if 0 <= deltaTheta <= math.pi / 2:
+                            if logging:
+                                print("found left goal")
+                        elif math.pi/2 <= deltaTheta <= 3 * math.pi/2:
+                            if logging:
+                                print("found behind goal")
+                        elif 3 * math.pi/2 <= deltaTheta:
+                            if logging:
+                                print("found right goal")
+                        state_machine["goalFindable"][0] = True
+                        goalCount += 1
+                if logging:
+                    print("finding goal: ", colors, pos_im)
+
         else:
-            leftSpeed = -0.05 * MAX_SPEED
-            rightSpeed = 0.05 * MAX_SPEED
-            cameraRecogObjects = camera.getRecognitionObjects()
-            # list of all objects detected
-            for obj in cameraRecogObjects:
-                # pos = i.get_position()
-                # x,y,z
-                colors = obj.get_colors()
-                pos_im = obj.get_position_on_image()
-                print("finding goal: ", colors, pos_im)
-                # [R,G,B]
-                # orange ball: [1.0, 0.54, 0.08]
+            leftSpeed = 0
+            rightSpeed = 0
+
+    else:
+        pass
 
     leftMotor.setVelocity(leftSpeed)
     rightMotor.setVelocity(rightSpeed)
